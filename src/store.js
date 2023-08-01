239e10b313ec84095a639347e3193d2db26b3eb3
Alpha Bah <alphabah@MacBook-Pro-de-Alpha.local>
2023-08-01 14:45:00 +0300

    final add

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
 import { getStorageItem, setStorageItem } from './utils.js';
-let store = [];
-const setupStore = () => {};
-const findProduct = () => {};
+let store = getStorageItem('store');
+const setupStore = (products) => {
+    store = products.map(product => {
+        const {id, fields} = product
+        const {featured, name, price, company, colors, image:img}= fields
+        const image = img[0].thumbnails.large.url;
+        return {id, featured, name, price, company, colors,image }
+    })
+    setStorageItem('store', store)
+};
+const findProduct = (id) => {
+    let product = store.find(item => item.id=== id )
+    return product
+};
 export { store, setupStore, findProduct };
